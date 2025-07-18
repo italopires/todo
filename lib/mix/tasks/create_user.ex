0f0e9464ce6807a69a6699a433fe9546d4e8b42f
Italo Pires <italonapalm@gmail.com>
2025-07-18 15:31:33 -0300

    Create task to create the first user

---
+++ lib/mix/tasks/create_user.ex
@@ -0,0 +1,39 @@
+# lib/mix/tasks/create_user.ex
+defmodule Mix.Tasks.CreateUser do
+  use Mix.Task
+
+  @shortdoc "Creates the first user with email and password"
+
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    email = prompt("Email")
+    password = prompt("Password")
+
+    create_user(email, password)
+  end
+
+  defp create_user(email, password) do
+    alias TodoApp.Accounts
+    alias Bcrypt
+
+    params = %{
+      "email" => email,
+      "password" => password,
+      "password_hash" => Bcrypt.hash_pwd_salt(password)
+    }
+
+    case Accounts.create_user(params) do
+      {:ok, user} ->
+        Mix.shell().info("✅ User created successfully: #{user.email}")
+
+      {:error, changeset} ->
+        Mix.shell().error("❌ Failed to create user:")
+        IO.inspect(changeset.errors)
+    end
+  end
+
+  defp prompt(label) do
+    IO.gets("#{label}: ") |> String.trim()
+  end
+end
