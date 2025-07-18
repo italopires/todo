885104584bd359355ec7cdbba49a7a5f0b90c8ec
Italo Pires <italonapalm@gmail.com>
2025-07-18 15:01:46 -0300

    Add bcrypt and guardian for jwt authentication

--- mix.exs
+++ mix.exs
@@ -56,7 +56,9 @@ defmodule TodoApp.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:bcrypt_elixir, "~> 3.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
